2fb40669b8e301dfe23a8de42fc793af8ba41218
Trenton Kress <trenton.kress@gmail.com>
2019-05-06 17:45:17 -0700

    Simplify a condition because that could have been called horrific.

--- src/com/makar/util/Interactions.java
+++ src/com/makar/util/Interactions.java
@@ -10,13 +10,12 @@ public class Interactions {
 	public static boolean walkOrTurnTo(Interactable object, String option, int chance) {
 		if (object.getVisibility() > 30 && object.interact(option)) {
 			return true;
-		} else {
-			if (object instanceof Locatable) {
-				if (Random.nextGaussian(0, 100) > chance) {
-					Camera.concurrentlyTurnTo((Locatable) object);
-				} else {
-					Movement.runToWithVariance((Locatable) object, (int) Math.round(Random.nextGaussian(1, 5, 3)));
-				}
+		} 
+		if (object instanceof Locatable) {
+			if (Random.nextGaussian(0, 100) > chance) {
+				Camera.concurrentlyTurnTo((Locatable) object);
+			} else {
+				Movement.runToWithVariance((Locatable) object, (int) Math.round(Random.nextGaussian(1, 5, 3)));
 			}
 		}
 		return false;
