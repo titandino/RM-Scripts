e96070ff398c3be765ccb4ee8cc666c3afaa13cb
Trenton Kress <trenton.kress@gmail.com>
2019-05-07 13:50:31 -0700

    Display banked crafting xp

--- src/com/makar/scripts/gemminer/GemMiner.java
+++ src/com/makar/scripts/gemminer/GemMiner.java
@@ -3,6 +3,7 @@ package com.makar.scripts.gemminer;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.List;
 
 import com.makar.scripts.gemminer.gui.GemGUIController;
 import com.makar.util.Interact;
@@ -15,6 +16,7 @@ import com.runemate.game.api.hybrid.input.Mouse.Button;
 import com.runemate.game.api.hybrid.local.Skill;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Bank;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Inventory;
+import com.runemate.game.api.hybrid.local.hud.interfaces.SpriteItem;
 import com.runemate.game.api.hybrid.location.Area;
 import com.runemate.game.api.hybrid.location.Coordinate;
 import com.runemate.game.api.hybrid.location.navigation.Landmark;
@@ -39,6 +41,33 @@ public class GemMiner extends LoopingBot implements EmbeddableUI, InventoryListe
 	
 	private static final Area MINE = Area.circular(new Coordinate(3298, 3298, 0), 5);
 	private static final Area PRECIOUS_MINE = Area.circular(new Coordinate(1182, 4510, 0), 5);
+	
+	private enum Uncut {
+		OPAL(1625, 15.0),
+		JADE(1627, 20),
+		RED_TOPAZ(1629, 25),
+		SAPPHIRE(1623, 50),
+		EMERALD(1621, 67),
+		RUBY(1619, 85),
+		DIAMOND(1617, 107.5),
+		DRAGONSTONE(1631, 137.5);
+		
+		public static Uncut forId(int id) {
+			for (Uncut u : Uncut.values()) {
+				if (u.id == id)
+					return u;
+			}
+			return null;
+		}
+		
+		private int id;
+		private double xp;
+		
+		private Uncut(int id, double xp) {
+			this.id = id;
+			this.xp = xp;
+		}
+	}
 
 	private enum Stage {
 		MINING, BANKING
@@ -51,6 +80,7 @@ public class GemMiner extends LoopingBot implements EmbeddableUI, InventoryListe
 	private HashMap<String, String> gemsMined = new HashMap<String, String>();
 	private double levelProgress = 0.0;
 	private String levelText = "No data";
+	private double craftingXpBanked = 0.0;
 
 	public GemMiner() {
 		controller = new GemGUIController(this);
@@ -123,6 +153,14 @@ public class GemMiner extends LoopingBot implements EmbeddableUI, InventoryListe
 				Execution.delayUntil(() -> Bank.isOpen(), 3500, 6700);
 				Bank.depositInventory();
 				Execution.delayUntil(() -> Inventory.isEmpty(), 3200, 5600);
+				
+				double xp = 0;
+				List<SpriteItem> gems = Bank.getItems(Uncut.OPAL.id, Uncut.JADE.id, Uncut.RED_TOPAZ.id, Uncut.SAPPHIRE.id, Uncut.EMERALD.id, Uncut.RUBY.id, Uncut.DIAMOND.id, Uncut.DRAGONSTONE.id).asList();
+				for (SpriteItem gem : gems) {
+					xp += Uncut.forId(gem.getId()).xp * gem.getQuantity();
+				}
+				craftingXpBanked = xp;
+				
 				if (Util.gaussian(0, 100, 75) < 30)
 					Keyboard.pressKey(KeyEvent.VK_ESCAPE);
 				else
@@ -143,7 +181,16 @@ public class GemMiner extends LoopingBot implements EmbeddableUI, InventoryListe
 		else
 			gemsMined.put(name, "" + (Integer.valueOf(current) + event.getQuantityChange()));
 		levelProgress = (double) (100 - Skill.MINING.getExperienceToNextLevelAsPercent()) / 100.0;
-		levelText = Skill.MINING.getCurrentLevel() + " (TNL: " + new DecimalFormat("#,###,###").format(Skill.MINING.getExperienceToNextLevel()) + ")";
+		levelText = new StringBuilder()
+				.append(Skill.MINING.getCurrentLevel())
+				.append(" (TNL: ")
+				.append(new DecimalFormat("#,###,###").format(Skill.MINING.getExperienceToNextLevel()))
+				.append(") Craft: ")
+				.append(new DecimalFormat("#,###,###").format(craftingXpBanked))
+				.append(" XP (")
+				.append(Util.getLevelForXp(craftingXpBanked + Skill.CRAFTING.getExperience()))
+				.append(")")
+				.toString();
 		Platform.runLater(() -> controller.update());
 	}
 	
