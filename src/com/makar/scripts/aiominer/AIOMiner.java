1e5ebf1e1b56c91300d01f98dc00524255709b59
Trenton Kress <trenton.kress@gmail.com>
2019-05-13 07:29:25 -0700

    Iron test

--- src/com/makar/scripts/aiominer/AIOMiner.java
+++ src/com/makar/scripts/aiominer/AIOMiner.java
@@ -5,7 +5,7 @@ import com.runemate.game.api.script.framework.task.TaskBot;
 public class AIOMiner extends TaskBot {
 	
 	public AIOMiner() {
-		add(Ore.TIN.getTasks());
+		add(Ore.IRON.getTasks());
 	}
 
 }
