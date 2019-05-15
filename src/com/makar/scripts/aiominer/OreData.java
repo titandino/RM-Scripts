492b1816ceae88bb2ebbe5a460e96f4397d1c996
Trenton Kress <trenton.kress@gmail.com>
2019-05-14 18:31:40 -0700

    Varrock west is much better iron

--- src/com/makar/scripts/aiominer/OreData.java
+++ src/com/makar/scripts/aiominer/OreData.java
@@ -13,18 +13,17 @@ import com.runemate.game.api.hybrid.location.navigation.basic.PredefinedPath;
 import com.runemate.game.api.script.framework.task.Task;
 
 public class OreData {
-	private static List<Coordinate> IRON_BANK_PATH = Arrays.asList(new Coordinate[] { new Coordinate(3048, 9782, 0), new Coordinate(3045, 9788, 0), new Coordinate(3044, 9795, 0), new Coordinate(3041, 9801, 0), new Coordinate(3040, 9807, 0), new Coordinate(3040, 9813, 0), new Coordinate(3040, 9818, 0), new Coordinate(3040, 9823, 0), new Coordinate(3040, 9829, 0), new Coordinate(3036, 9831, 0), new Coordinate(3033, 9831, 0), new Coordinate(3030, 9828, 0), new Coordinate(3027, 9824, 0), new Coordinate(3025, 9820, 0), new Coordinate(3023, 9816, 0), new Coordinate(3020, 9813, 0), new Coordinate(3016, 9812, 0), new Coordinate(3014, 9812, 0) });
+	private static List<Coordinate> IRON_BANK_PATH = Arrays.asList(new Coordinate[] { new Coordinate(3181, 3372, 0), new Coordinate(3183, 3370, 0), new Coordinate(3186, 3371, 0), new Coordinate(3186, 3374, 0), new Coordinate(3184, 3376, 0), new Coordinate(3179, 3376, 0), new Coordinate(3179, 3380, 0), new Coordinate(3177, 3384, 0), new Coordinate(3175, 3388, 0), new Coordinate(3173, 3392, 0), new Coordinate(3171, 3396, 0), new Coordinate(3169, 3400, 0), new Coordinate(3171, 3403, 0), new Coordinate(3171, 3407, 0), new Coordinate(3172, 3410, 0), new Coordinate(3172, 3413, 0), new Coordinate(3172, 3416, 0), new Coordinate(3172, 3419, 0), new Coordinate(3172, 3422, 0), new Coordinate(3173, 3424, 0), new Coordinate(3175, 3426, 0), new Coordinate(3178, 3425, 0), new Coordinate(3181, 3425, 0), new Coordinate(3183, 3424, 0), new Coordinate(3187, 3424, 0) });
 	private static List<Coordinate> IRON_MINE_PATH = Util.reverseList(IRON_BANK_PATH);
 
 	public enum Ore {
 		COPPER(new QuickDepositAll(), new MineOre("Copper rock")), 
 		
-		TIN(new QuickDepositAll(), new MineOre("Tin rock")), 
+		TIN(new QuickDepositAll(), new MineOre("Tin rock")),
 		
-		IRON(/*new HideFromCombat(new Coordinate(3048, 9782, 0)),*/
-				new TravelTo(new Coordinate(3013, 9812, 0), PredefinedPath.create(IRON_BANK_PATH), () -> Inventory.isFull()),
+		IRON(new TravelTo(new Coordinate(3187, 3424, 0), PredefinedPath.create(IRON_BANK_PATH), () -> Inventory.isFull()),
 				new QuickDepositAll(), 
-				new TravelTo(new Coordinate(3049, 9782, 0), PredefinedPath.create(IRON_MINE_PATH), () -> !Inventory.isFull()), 
+				new TravelTo(new Coordinate(3181, 3373, 0), PredefinedPath.create(IRON_MINE_PATH), () -> !Inventory.isFull()), 
 				new MineOre("Iron rock"));
 
 		private Task[] tasks;
