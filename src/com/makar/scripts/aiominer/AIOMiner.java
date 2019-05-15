83fc2b8391ba5e2f0c61554ecc06158d9f4bb95c
Trenton Kress <trenton.kress@gmail.com>
2019-05-14 20:05:00 -0700

    Cache collision flags

--- src/com/makar/scripts/aiominer/AIOMiner.java
+++ src/com/makar/scripts/aiominer/AIOMiner.java
@@ -1,12 +1,14 @@
 package com.makar.scripts.aiominer;
 
 import com.makar.scripts.aiominer.OreData.Ore;
+import com.runemate.game.api.hybrid.region.Region;
 import com.runemate.game.api.script.framework.task.TaskBot;
 
 public class AIOMiner extends TaskBot {
 	
 	public AIOMiner() {
 		add(Ore.IRON.getTasks());
+		Region.cacheCollisionFlags(true);
 	}
 
 }
