6ccb1c936e239391a7b092258bcad2820ffc002d
Trenton Kress <trenton.kress@gmail.com>
2019-05-15 23:46:22 -0700

    Mithril mining

--- src/com/makar/scripts/aiominer/OreData.java
+++ src/com/makar/scripts/aiominer/OreData.java
@@ -14,7 +14,10 @@ import com.runemate.game.api.script.framework.task.Task;
 public class OreData {
 	private static List<Coordinate> IRON_BANK_PATH = Arrays.asList(new Coordinate[] { new Coordinate(3181, 3372, 0), new Coordinate(3183, 3370, 0), new Coordinate(3186, 3371, 0), new Coordinate(3186, 3374, 0), new Coordinate(3184, 3376, 0), new Coordinate(3179, 3376, 0), new Coordinate(3179, 3380, 0), new Coordinate(3177, 3384, 0), new Coordinate(3175, 3388, 0), new Coordinate(3173, 3392, 0), new Coordinate(3171, 3396, 0), new Coordinate(3169, 3400, 0), new Coordinate(3171, 3403, 0), new Coordinate(3171, 3407, 0), new Coordinate(3172, 3410, 0), new Coordinate(3172, 3413, 0), new Coordinate(3172, 3416, 0), new Coordinate(3172, 3419, 0), new Coordinate(3172, 3422, 0), new Coordinate(3173, 3424, 0), new Coordinate(3175, 3426, 0), new Coordinate(3178, 3425, 0), new Coordinate(3181, 3425, 0), new Coordinate(3183, 3424, 0), new Coordinate(3187, 3424, 0) });
 	private static List<Coordinate> IRON_MINE_PATH = Util.reverseList(IRON_BANK_PATH);
-
+	
+	private static List<Coordinate> MITHRIL_BANK_PATH = Arrays.asList(new Coordinate[] {new Coordinate(3184, 3376, 0), new Coordinate(3179, 3376, 0), new Coordinate(3179, 3380, 0), new Coordinate(3177, 3384, 0), new Coordinate(3175, 3388, 0), new Coordinate(3173, 3392, 0), new Coordinate(3171, 3396, 0), new Coordinate(3169, 3400, 0), new Coordinate(3171, 3403, 0), new Coordinate(3171, 3407, 0), new Coordinate(3172, 3410, 0), new Coordinate(3172, 3413, 0), new Coordinate(3172, 3416, 0), new Coordinate(3172, 3419, 0), new Coordinate(3172, 3422, 0), new Coordinate(3173, 3424, 0), new Coordinate(3175, 3426, 0), new Coordinate(3178, 3425, 0), new Coordinate(3181, 3425, 0), new Coordinate(3183, 3424, 0), new Coordinate(3187, 3424, 0)});
+	private static List<Coordinate> MITHRIL_MINE_PATH = Util.reverseList(MITHRIL_BANK_PATH);
+	
 	public enum Ore {
 		COPPER(new QuickDepositAll(), new MineOre("Copper rock")),
 		
@@ -23,7 +26,12 @@ public class OreData {
 		IRON(new TravelTo(new Coordinate(3187, 3424, 0), IRON_BANK_PATH, () -> Inventory.isFull()),
 				new QuickDepositAll(), 
 				new TravelTo(new Coordinate(3181, 3373, 0), IRON_MINE_PATH, () -> !Inventory.isFull()), 
-				new MineOre("Iron rock"));
+				new MineOre("Iron rock")),
+		
+		MITHRIL(new TravelTo(new Coordinate(3187, 3424, 0), MITHRIL_BANK_PATH, () -> Inventory.isFull()),
+				new QuickDepositAll(), 
+				new TravelTo(new Coordinate(3181, 3373, 0), MITHRIL_MINE_PATH, () -> !Inventory.isFull()), 
+				new MineOre("Mithril rock"));
 
 		private Task[] tasks;
 
--- src/com/makar/util/Interact.java
+++ src/com/makar/util/Interact.java
@@ -5,42 +5,40 @@ import java.util.regex.Pattern;
 import com.runemate.game.api.hybrid.entities.details.Interactable;
 import com.runemate.game.api.hybrid.entities.details.Locatable;
 import com.runemate.game.api.hybrid.local.Camera;
-import com.runemate.game.api.hybrid.local.hud.Menu;
-import com.runemate.game.api.hybrid.local.hud.MenuItem;
 import com.runemate.game.api.hybrid.util.calculations.Random;
 
 public class Interact {
-	
+
 	public static boolean walkOrTurnTo(Interactable object, Pattern option) {
 		return walkOrTurnTo(object, option, Util.gaussian(0, 500, 150), true);
 	}
-	
+
 	public static boolean walkOrTurnTo(Interactable object, Pattern option, int chance) {
 		return walkOrTurnTo(object, option, chance, true);
 	}
-	
+
 	public static boolean walkOrTurnTo(Interactable object, String option) {
 		return walkOrTurnTo(object, option, Util.gaussian(0, 500, 150), false);
 	}
-	
+
 	public static boolean walkOrTurnTo(Interactable object, String option, int chance) {
 		return walkOrTurnTo(object, option, chance, false);
 	}
-	
+
 	private static boolean walkOrTurnTo(Interactable object, Object option, int chance, boolean pattern) {
 		if (object == null)
 			return false;
-		if (Menu.isOpen()) {
-			MenuItem item;
-            if ((item = pattern ? Menu.getItem((Pattern) option) : Menu.getItem((String) option)) != null) {
-                item.click();
-            } else {
-                Menu.close();
-            }
-		} else {
+//		if (Menu.isOpen()) { //BROKEN ON RS3. #incredibleapi
+//			MenuItem item;
+//			if ((item = pattern ? Menu.getItem((Pattern) option) : Menu.getItem((String) option)) != null) {
+//				item.click();
+//			} else {
+//				Menu.close(); //PRESSES ESCAPE TO CLOSE AS IF IT IS A REAL INTERFACE :) #playersense #breathtakingcode #greatantiban
+//			}
+//		} else {
 			if (object.isVisible() && (pattern ? object.interact((Pattern) option) : object.interact((String) option))) {
 				return true;
-			} 
+			}
 			if (object instanceof Locatable) {
 				if (Random.nextGaussian(0, 100) < chance) {
 					Camera.concurrentlyTurnTo((Locatable) object);
@@ -49,7 +47,7 @@ public class Interact {
 				}
 				return true;
 			}
-		}
+//		}
 		return false;
 	}
 
